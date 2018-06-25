1ec6b5f0187c2f93e42903d18791099d52c34bf9
yuz <zhuchunyu@cloud-star.com>
2018-06-25 17:19:20 +0800

    first commit

---
+++ src/main/java/org/activiti/ResumeService.java
@@ -0,0 +1,15 @@
+package org.activiti;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author yuz
+ */
+@Component
+public class ResumeService {
+
+    public void storeResume() {
+        System.out.println("Storing resume ...");
+    }
+
+}
